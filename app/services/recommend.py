abac2fb0ee05c9c81f4bb10a4d37ee1605d4bc30
AshTree007 <ashwin.gdr1@gmail.com>
2026-03-31 18:14:15 -0500

    Feature implemetation: Now sorts by both distance and restriction Addition: Added all locations to locations.txt with additional info about each place

--- app/services/recommend.py
+++ app/services/recommend.py
@@ -4,52 +4,110 @@ from datetime import datetime, timezone
 
 from math import sqrt
 
-import geopy
-
 from rich import print
+
+from geopy.distance import geodesic
 
 
 class recommendation:
-    def __init__(self, current_location: tuple, center_of_interst: tuple, radius: float, restrictions: list, time_available: float):
+
+    TAMU_BOUNDS = {
+    'north': 30.643,  # ~1 mile north
+    'south': 30.595,  # ~1 mile south
+    'east': -96.315,  # ~1 mile east
+    'west': -96.380   # ~1 mile west
+    }
+    
+
+    def __init__(self, current_location: tuple, center_of_interst: tuple, radius: float, user_id: str):
         self.current_location = current_location
         self.center = center_of_interst
         self.radius = radius
-        self.restrictions = restrictions
-        self.time = time_available
+        self.restriction, self.time = self._get_details(user_id)
         self.all_food_loc = self._get_food_locs()
 
-    def _get_food_locs(self):
+
+    def _get_food_locs(self) -> list:
         food_locations = []
         with open("locations.txt", "r") as f:
             for location in f:
-                loc = location.strip().split(",")
-                food_locations.append({loc[0]:(float(loc[1]),float(loc[2]))})
+                loc = location.strip().split(", ")
+                food_locations.append({loc[0]:[(float(loc[1]),float(loc[2])), loc[3], loc[4]]})
         
         return food_locations
-
-    def _calc_distance(self, point_1: tuple, point_2: tuple):
-        return sqrt((point_2[0]-point_1[0])**2+(point_2[1]-point_1[1])**2)
     
-    def _within_radius(self, point_1: tuple, point_2: tuple):
+
+    def _get_details(self, user_id) -> tuple[list, float]:
+
+        restriction = "vegetarian"
+        time = None
+        #####################################################################################################
+        #########            TODO: Add logic to get the restrictions of the user and                #########            
+        #########                   the time they have to get food                                  #########
+        #####################################################################################################
+
+        return(restriction,time)    
+
+
+    def _calc_distance(self, point_1: tuple, point_2: tuple) -> float:
+        return geodesic(point_1, point_2).m
+    
+
+    def _within_radius(self, point_1: tuple, point_2: tuple) -> bool:
         return self._calc_distance(point_1, point_2) <= self.radius
+    
 
-    def recommend(self):
-        recom_locs = []
+    def _is_on_campus(self) -> bool:
+        lat, long = self.current_location
+        return self.TAMU_BOUNDS['south'] <= lat <= self.TAMU_BOUNDS['north'] and self.TAMU_BOUNDS['west'] <= long <= self.TAMU_BOUNDS['east']
+    
+
+    def _filter_by_restriction(self, locations: list) -> list:
         
+        new_locs = []
+
+        if self.restriction == "vegetarian":
+            for location in locations:
+                if location["restriction"].startswith("vegetarian") or location["restriction"].startswith("vegan"):
+                    new_locs.append(location)
+        
+        elif self.restriction == "vegan":
+            for location in locations:
+                if location["restriction"].startswith("vegan"):
+                    new_locs.append(location)
+
+        
+        return new_locs
+    
+
+    def recommend(self) -> list:
+        recom_locs = []
+
+        if not self._is_on_campus():
+            raise ValueError("Location is out of bounds")
+
         for location in self.all_food_loc:
-            loc, coord =  next(iter(location.items()))
+            name, (coord, cuisine, restriction) = next(iter(location.items()))
             if self._within_radius(self.center, coord):
-                recom_locs.append(loc)
+                recom_locs.append({'name': name, 'cuisine': cuisine, 'restriction': restriction, 'distance': self._calc_distance(self.current_location, coord)})
 
-        return recom_locs        
+        #     TODO: Finish
+        #
+        #        1: Get the recommended locations from S3
 
+        recom_locs = self._filter_by_restriction(recom_locs)
 
+        print(recom_locs)
+
+        recom_locs.sort(key=lambda x: x['distance'])
+
+        return recom_locs
 
 
     
 
     
-    
+
 if __name__ == "__main__":
-    new_rec = recommendation((1,1), (1,1), 2.0, ["none"], 9999)
+    new_rec = recommendation((30.6123, -96.3414),(30.6123, -96.3414), 400.0, "user1")
     print(new_rec.recommend())
