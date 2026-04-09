b187c625cc965d4f104c1e2d9a3d8d4ba9c6bb81
AshTree007 <ashwin.gdr1@gmail.com>
2026-04-09 13:56:52 -0500

    Refactor: converted txt file of locations to a python file for faster access

---
+++ app/services/locations.py
@@ -0,0 +1,387 @@
+ALL_FOOD_LOCATIONS = [
+    {
+        "name": "The Commons Dining Hall (South Campus)",
+        "latitude": 30.6155,
+        "longitude": -96.3361,
+        "cuisine": "Buffet",
+        "restriction": "meat_mixed"
+    },
+    {
+        "name": "Sbisa Dining Hall (North Campus)",
+        "latitude": 30.6171,
+        "longitude": -96.3438,
+        "cuisine": "Buffet",
+        "restriction": "meat_mixed"
+    },
+    {
+        "name": "Duncan Dining Hall (South Campus/Quad)",
+        "latitude": 30.6121,
+        "longitude": -96.3355,
+        "cuisine": "Buffet",
+        "restriction": "meat_mixed"
+    },
+    {
+        "name": "1876 Burgers - Sbisa Complex",
+        "latitude": 30.6171,
+        "longitude": -96.3441,
+        "cuisine": "American",
+        "restriction": "meat_beef"
+    },
+    {
+        "name": "Aggie Express - Hullabaloo",
+        "latitude": 30.6166,
+        "longitude": -96.3464,
+        "cuisine": "Convenience Store",
+        "restriction": "meat_mixed"
+    },
+    {
+        "name": "Chick-Fil-A - Sbisa Underground Food Court",
+        "latitude": 30.6171,
+        "longitude": -96.3438,
+        "cuisine": "American Fast Food",
+        "restriction": "meat_chicken"
+    },
+    {
+        "name": "Copperhead Jack's - Sbisa Complex",
+        "latitude": 30.6171,
+        "longitude": -96.3439,
+        "cuisine": "Southwestern",
+        "restriction": "vegan"
+    },
+    {
+        "name": "Einstein Bros. Bagels - Sbisa Complex",
+        "latitude": 30.6172,
+        "longitude": -96.3442,
+        "cuisine": "Breakfast",
+        "restriction": "vegetarian"
+    },
+    {
+        "name": "Houston Street Subs - Underground Food Court",
+        "latitude": 30.6171,
+        "longitude": -96.3438,
+        "cuisine": "Deli",
+        "restriction": "vegan"
+    },
+    {
+        "name": "Bagel Block",
+        "latitude": 30.6195,
+        "longitude": -96.3421,
+        "cuisine": "Coffee Shop",
+        "restriction": "vegetarian"
+    },
+    {
+        "name": "Pizza @ Underground",
+        "latitude": 30.6171,
+        "longitude": -96.3438,
+        "cuisine": "Italian",
+        "restriction": "vegetarian"
+    },
+    {
+        "name": "Starbucks Coffee - Hullabaloo",
+        "latitude": 30.6163,
+        "longitude": -96.3460,
+        "cuisine": "Coffee Shop",
+        "restriction": "vegetarian"
+    },
+    {
+        "name": "Smoothie King - Sbisa Underground Food Court",
+        "latitude": 30.6171,
+        "longitude": -96.3438,
+        "cuisine": "Smoothies",
+        "restriction": "vegan"
+    },
+    {
+        "name": "Abu Omar Halal - MSC",
+        "latitude": 30.6122,
+        "longitude": -96.3413,
+        "cuisine": "Mediterranean",
+        "restriction": "meat_chicken"
+    },
+    {
+        "name": "Aggie Express - Pavilion",
+        "latitude": 30.6166,
+        "longitude": -96.3382,
+        "cuisine": "Convenience Store",
+        "restriction": "meat_mixed"
+    },
+    {
+        "name": "Cabo Grill - MSC",
+        "latitude": 30.6122,
+        "longitude": -96.3413,
+        "cuisine": "Tex-Mex",
+        "restriction": "vegan"
+    },
+    {
+        "name": "Chick-Fil-A - MSC Food Court",
+        "latitude": 30.6122,
+        "longitude": -96.3413,
+        "cuisine": "American Fast Food",
+        "restriction": "meat_chicken"
+    },
+    {
+        "name": "Houston Street Subs - MSC",
+        "latitude": 30.6122,
+        "longitude": -96.3413,
+        "cuisine": "Deli",
+        "restriction": "vegan"
+    },
+    {
+        "name": "Market at Lamar St.",
+        "latitude": 30.6123,
+        "longitude": -96.3441,
+        "cuisine": "Convenience Store",
+        "restriction": "meat_mixed"
+    },
+    {
+        "name": "Moore Family Creamery",
+        "latitude": 30.6105,
+        "longitude": -96.3373,
+        "cuisine": "Dessert",
+        "restriction": "vegetarian"
+    },
+    {
+        "name": "Panda Express - MSC",
+        "latitude": 30.6122,
+        "longitude": -96.3413,
+        "cuisine": "Asian Fast Food",
+        "restriction": "meat_chicken"
+    },
+    {
+        "name": "Rev's American Grill - MSC",
+        "latitude": 30.6122,
+        "longitude": -96.3413,
+        "cuisine": "American",
+        "restriction": "meat_beef"
+    },
+    {
+        "name": "Starbucks Coffee - Evans Library",
+        "latitude": 30.6172,
+        "longitude": -96.3391,
+        "cuisine": "Coffee Shop",
+        "restriction": "vegetarian"
+    },
+    {
+        "name": "Shake Smart - MSC",
+        "latitude": 30.6122,
+        "longitude": -96.3413,
+        "cuisine": "Smoothies",
+        "restriction": "vegetarian"
+    },
+    {
+        "name": "Spin 'N Stone Pizza - MSC",
+        "latitude": 30.6122,
+        "longitude": -96.3413,
+        "cuisine": "Pizza",
+        "restriction": "meat_mixed"
+    },
+    {
+        "name": "The University Club",
+        "latitude": 30.6131,
+        "longitude": -96.3399,
+        "cuisine": "Buffet",
+        "restriction": "meat_mixed"
+    },
+    {
+        "name": "Whoop Coop",
+        "latitude": 30.6168,
+        "longitude": -96.3381,
+        "cuisine": "American",
+        "restriction": "meat_chicken"
+    },
+    {
+        "name": "Aggie Express - Commons",
+        "latitude": 30.6152,
+        "longitude": -96.3359,
+        "cuisine": "Convenience Store",
+        "restriction": "meat_mixed"
+    },
+    {
+        "name": "Starbucks Coffee - The Quad",
+        "latitude": 30.6136,
+        "longitude": -96.3375,
+        "cuisine": "Coffee Shop",
+        "restriction": "vegetarian"
+    },
+    {
+        "name": "Houston Street Subs - Southside",
+        "latitude": 30.6150,
+        "longitude": -96.3359,
+        "cuisine": "Deli",
+        "restriction": "vegan"
+    },
+    {
+        "name": "Azimuth Cafe - Langford",
+        "latitude": 30.6190,
+        "longitude": -96.3376,
+        "cuisine": "Cafe",
+        "restriction": "vegetarian"
+    },
+    {
+        "name": "Houston Street Subs - Polo Garage",
+        "latitude": 30.6225,
+        "longitude": -96.3380,
+        "cuisine": "Deli",
+        "restriction": "vegan"
+    },
+    {
+        "name": "Market at Polo Garage",
+        "latitude": 30.6233,
+        "longitude": -96.3378,
+        "cuisine": "Convenience Store",
+        "restriction": "meat_mixed"
+    },
+    {
+        "name": "Panda Express - Polo Garage",
+        "latitude": 30.6224,
+        "longitude": -96.3378,
+        "cuisine": "Asian Fast Food",
+        "restriction": "meat_chicken"
+    },
+    {
+        "name": "Salata",
+        "latitude": 30.6232,
+        "longitude": -96.3376,
+        "cuisine": "Healthy",
+        "restriction": "vegan"
+    },
+    {
+        "name": "Shake Smart - Polo Garage",
+        "latitude": 30.6227,
+        "longitude": -96.3382,
+        "cuisine": "Smoothies",
+        "restriction": "vegetarian"
+    },
+    {
+        "name": "Starbucks Coffee - Zachry",
+        "latitude": 30.6210,
+        "longitude": -96.3406,
+        "cuisine": "Coffee Shop",
+        "restriction": "vegetarian"
+    },
+    {
+        "name": "Reynolds and Reynolds Cafe",
+        "latitude": 30.6113,
+        "longitude": -96.3497,
+        "cuisine": "Cafe",
+        "restriction": "vegetarian"
+    },
+    {
+        "name": "Chick-fil-A - West Campus Food Hall",
+        "latitude": 30.6102,
+        "longitude": -96.3486,
+        "cuisine": "American Fast Food",
+        "restriction": "meat_chicken"
+    },
+    {
+        "name": "Copperhead Jack's - West Campus Food Hall",
+        "latitude": 30.6102,
+        "longitude": -96.3485,
+        "cuisine": "Southwestern",
+        "restriction": "vegan"
+    },
+    {
+        "name": "Creekside Market",
+        "latitude": 30.6078,
+        "longitude": -96.3538,
+        "cuisine": "Convenience Store",
+        "restriction": "meat_mixed"
+    },
+    {
+        "name": "The Kitchen - Creekside Market",
+        "latitude": 30.6078,
+        "longitude": -96.3538,
+        "cuisine": "American",
+        "restriction": "meat_mixed"
+    },
+    {
+        "name": "Spin n' Stone Pizza - Creekside Market",
+        "latitude": 30.6076,
+        "longitude": -96.3539,
+        "cuisine": "Pizza",
+        "restriction": "meat_mixed"
+    },
+    {
+        "name": "Spin 'n Stone Pasta - Creekside Market",
+        "latitude": 30.6078,
+        "longitude": -96.3538,
+        "cuisine": "Italian",
+        "restriction": "vegetarian"
+    },
+    {
+        "name": "Health Science Center Cafe",
+        "latitude": 30.5984,
+        "longitude": -96.3950,
+        "cuisine": "Cafe",
+        "restriction": "meat_mixed"
+    },
+    {
+        "name": "Houston Street Deli - RELLIS",
+        "latitude": 30.6413,
+        "longitude": -96.4668,
+        "cuisine": "Deli",
+        "restriction": "vegan"
+    },
+    {
+        "name": "Wild Blue Sushi - Creekside Market",
+        "latitude": 30.6078,
+        "longitude": -96.3538,
+        "cuisine": "Asian",
+        "restriction": "meat_fish"
+    },
+    {
+        "name": "Houston Street Subs - West Campus Food Hall",
+        "latitude": 30.6102,
+        "longitude": -96.3486,
+        "cuisine": "Deli",
+        "restriction": "vegan"
+    },
+    {
+        "name": "Market - Ag Cafe",
+        "latitude": 30.6108,
+        "longitude": -96.3490,
+        "cuisine": "Convenience Store",
+        "restriction": "meat_mixed"
+    },
+    {
+        "name": "Shake Smart - West Campus Food Hall",
+        "latitude": 30.6102,
+        "longitude": -96.3487,
+        "cuisine": "Smoothies",
+        "restriction": "vegetarian"
+    },
+    {
+        "name": "Starbucks - Ag Cafe",
+        "latitude": 30.6108,
+        "longitude": -96.3490,
+        "cuisine": "Coffee Shop",
+        "restriction": "vegetarian"
+    },
+    {
+        "name": "Shake Smart- Rec Center",
+        "latitude": 30.6070,
+        "longitude": -96.3429,
+        "cuisine": "Smoothies",
+        "restriction": "vegetarian"
+    },
+    {
+        "name": "The 41st Club - Bush Library",
+        "latitude": 30.5976,
+        "longitude": -96.3524,
+        "cuisine": "Convenience Store",
+        "restriction": "meat_mixed"
+    },
+    {
+        "name": "Vet Med Cafe",
+        "latitude": 30.6117,
+        "longitude": -96.3573,
+        "cuisine": "Cafe",
+        "restriction": "meat_mixed"
+    },
+    {
+        "name": "Market Express - Business Library (BLCC)",
+        "latitude": 30.6114,
+        "longitude": -96.3501,
+        "cuisine": "Convenience Store",
+        "restriction": "meat_mixed"
+    }
+]
